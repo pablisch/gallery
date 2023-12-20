0e977a5f45ef2073d3f19a0e7948c789816faff3
Pablo <pablito@me.com>
2023-12-20 19:45:54 +0000

    Slight refactor of Feed and ImagePanel

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -42,9 +42,7 @@ const Feed = () => {
         gutter="200px"
       >
         {imageData.length > 0 && [...imageData].reverse().map((image, index) => (
-          <div key={image._id} className={`image-card custom-class-${index}`}>
-            <ImagePanel image={image} />
-          </div>
+            <ImagePanel key={image._id} image={image} />
         ))}
       </Masonry>
     </div>
