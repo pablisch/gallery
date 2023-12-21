319477ea783ee2ebc572ba5971b114c27d5bc726
Pablo <pablito@me.com>
2023-12-21 20:08:14 +0000

    IMplemented basic functionality of hover by displaying the avatar and username

--- frontend/src/components/ImagePanel.jsx
+++ frontend/src/components/ImagePanel.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './ImagePanel.css';
+import { set } from 'date-fns';
 
 const ImagePanel = ({ image, setSelectedImage }) => {
   const [isHovered, setIsHovered] = useState(false);
+
+  let avatar = null;
 
   const navigate = useNavigate();
 
@@ -14,16 +17,47 @@ const ImagePanel = ({ image, setSelectedImage }) => {
     navigate(`/images/${image._id}`)
   };
 
+  avatar = image.userAvatar?.length > 1 ?
+    <img className='hover-icon icon avatar-icon' src={image.userAvatar} alt="avatar and settings icon" /> :
+    <div id="image-post-avatar-container" className="hover-icon icon avatar-letter-outer-container avatar-letter-container">
+        <h1 >{image.userAvatar}</h1>
+    </div>;
+  
+  const onHoverStart = () => {
+      setIsHovered(true);
+  }
+
+  const onHoverEnd = () => {
+    setIsHovered(false);
+  }
+
   return (
-    <img
-      id={`image-${image.src}`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      onClick={handleClick}
-      src={image.src}
-      alt={image.altText}
-      className={isHovered ? "selected image-card" : "image-card"}
-    />
+    <div className="image-card-container">
+      <img
+        id={`image-${image.src}`}
+        onMouseEnter={onHoverStart}
+        onMouseLeave={onHoverEnd}
+        onClick={handleClick}
+        src={image.src}
+        alt={image.altText}
+        className={isHovered ? 'selected image-card' : 'image-card'}
+      />
+      {isHovered && (
+        <div className="hover-icon-container"
+          onMouseEnter={onHoverStart}
+          onMouseLeave={onHoverEnd}
+        >
+          {image.userAvatar && image.userAvatar?.length > 1 ? (
+            <img className="hover-icon icon avatar-icon" src={image.userAvatar} alt="avatar and settings icon" />
+          ) : (
+            <div id="image-post-avatar-container" className="hover-icon icon avatar-letter-outer-container avatar-letter-container">
+              <h1>{image.userAvatar}</h1>
+            </div>
+          )}
+          {image.username ? <p className="hover-username">{image.username}</p> : <p></p>}
+        </div>
+      )}
+    </div>
   );
 };
 
